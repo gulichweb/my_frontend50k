8afbf53f63462fb7630a88feee2595bbfe19f9ff
gulichweb <gugomaderova@gmail.com>
2022-09-16 06:13:48 -0400

    all

---
+++ ДЗ-27/js/script.js
@@ -0,0 +1,78 @@
+
+// "Use strict";
+
+// let object = {
+//     name: "Zac",
+//     surname: "Miken",
+//     age: 25,
+//     date: 2020,
+//     city: "Bishkek",
+//     work: "programmer"
+
+// };
+
+// console.log(" Name: " + object.name +  " Surname: " + object.surname + " Age: " + object.age + " Date: " +  object.date + " city: " + object.city + " Work: " + object.work);
+
+
+
+// /*массивы */
+
+// let animals = ["Тигр", "Волк", "Слон", "Пантера", "Кенгуру"];
+// console.log(animals[2]);
+
+
+// /*переборы */
+
+let arr = ["Телефон", "Ноутбук", "Музыка", "Уроки"];
+console.log(arr.length);
+
+
+// /*перебираемый объект */
+
+
+for (let char of "1 2 3 4" ) {
+
+console.log( char );
+
+}
+
+/*ДЗ 25 */
+
+
+/* 1) */
+
+var person = prompt('Введите имя:', '');
+
+alert('Ваше имя: ' + person + '');
+
+var person = prompt('Введите Фамилию:', '');
+
+alert('Ваше фамилия: ' + person + '');
+
+
+
+/* 2) */
+
+
+var number =  prompt('ведите два числа:', '' );
+
+alert (number + number );
+
+
+/* 3)* */
+
+
+
+var sum = prompt('Ведите два числа:', '');
+
+alert(sum * sum);
+
+
+
+
+
+    
+    
+
+
+
