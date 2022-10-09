63c569b589b9c0a0b732fdeb35c6bb3d4ee5abb1
gulichweb <gugomaderova@gmail.com>
2022-10-09 15:42:05 -0400

    good

---
+++ ДЗ-34/script/add_to_cart.js
@@ -0,0 +1,96 @@
+
+// /*заголовок*/ 
+
+// let title = document.getElementsByClassName("title");
+// console.log(title[1].innerText);
+
+// /*инфо о картинке*/
+
+// let img = document.getElementsByTagName("img");
+// console.log(img[2].src);
+
+// /*текст*/
+
+// let footer = document.getElementsByClassName("mt-4");
+// console.log(footer[1].innerText);
+
+// /*товар - цена*/
+
+// let price = document.getElementsByClassName("price");
+// console.log(price[1].innerText);
+
+// картинки
+
+// let src = document.querySelectorAll(".product > .img");
+// console.log(src[0]);
+// console.log(src[1]);
+// console.log(src[2]);
+// console.log(src[3]);
+// console.log(src[4]);
+
+
+// var elements = document.getElementsByClassName("add_cart");
+
+// var myFunction = function() {
+//     var attribute = this.parentNode.parentNode.parentNode.querySelectorAll(".title");
+//     console.log(attribute[0].innerText);
+// };
+
+// for (var i = 0; i < elements.length; i++) {
+//     elements[i].addEventListener('click', myFunction, false);
+// }
+
+// ДЗ-33 добавление товаров в корзину
+
+
+   let my_info = document.getElementsByClassName("add_cart");
+
+    console.log(my_info);
+
+               
+  
+
+   let myFunction = function() {
+
+
+   let title = this.parentNode.parentNode.parentNode.querySelectorAll(".body_product p");
+   let price = this.parentNode.parentNode.parentNode.querySelectorAll(".price del");
+   let img = this.parentNode.parentNode.parentNode.parentNode.querySelectorAll( "img");
+
+   let creat_li = document.createElement('li');
+    console.log();
+    console.log();
+    console.log();
+
+
+    creat_li.innerHTML = '<div class="left">\
+                                <div class="left">\
+                                  <img src="'+img[0].src+'" alt="">\
+                                </div>\
+                                <div class="right m-l-10">\
+                                    <p>'+title[0].innerText+'</p>\
+                                    <p>'+price[0].innerText+'<del>'+price[0].innerText+'</del></p>\
+                                </div>\
+                            </div>\
+                            <div class="fa-solid">\
+                                <i class="fa-solid fa-trash-can"></i>\
+                            </div>\
+                            ';
+
+
+     let list_cart = document.querySelector('.my_shope  ul');
+         list_cart.appendChild(creat_li);
+
+       alert("Товары успешно добавлены!");
+
+
+
+                          };
+
+  for (var i = 0; i < my_info.length; i++) {
+    my_info[i].addEventListener('click', myFunction, false);
+
+ }; 
+                 
+                 
+ 
