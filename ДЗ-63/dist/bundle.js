601fb5baf0654a367f4a9ebf408513da88013ed7
gulichweb <gugomaderova@gmail.com>
2023-01-08 12:48:05 -0500

    Babel

---
+++ ДЗ-63/dist/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/scripts.js":
+/*!***********************!*\
+  !*** ./js/scripts.js ***!
+  \***********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: [BABEL] C:\\OSPanel\\domains\\ajax\\js\\scripts.js: @babel/preset-env: 'useBuiltins' is not a valid top-level option.\n- Did you mean 'useBuiltIns'? (While processing: \"C:\\\\OSPanel\\\\domains\\\\ajax\\\\node_modules\\\\@babel\\\\preset-env\\\\lib\\\\index.js\")\n    at OptionValidator.validateTopLevelOptions (C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\helper-validator-option\\lib\\validator.js:20:15)\n    at normalizeOptions (C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\preset-env\\lib\\normalize-options.js:108:5)\n    at C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\preset-env\\lib\\index.js:222:37\n    at C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\helper-plugin-utils\\lib\\index.js:31:12\n    at async (C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\core\\lib\\gensync-utils\\async.js:36:33)\n    at async (C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:186:15)\n    at C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:216:13\n    at Generator.next (<anonymous>)\n    at C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\core\\lib\\config\\full.js:181:21\n    at Generator.next (<anonymous>)\n    at Function.<anonymous> (C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\core\\lib\\gensync-utils\\async.js:21:3)\n    at Generator.next (<anonymous>)\n    at step (C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:269:25)\n    at evaluateAsync (C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:291:5)\n    at Function.errback (C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:113:7)\n    at errback (C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\core\\lib\\gensync-utils\\async.js:66:18)\n    at async (C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:188:17)\n    at onFirstPause (C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:216:13)\n    at Generator.next (<anonymous>)\n    at cachedFunction (C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\core\\lib\\config\\caching.js:52:46)\n    at cachedFunction.next (<anonymous>)\n    at loadPresetDescriptor (C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\core\\lib\\config\\full.js:294:43)\n    at loadPresetDescriptor.next (<anonymous>)\n    at recursePresetDescriptors (C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\core\\lib\\config\\full.js:77:31)\n    at recursePresetDescriptors.next (<anonymous>)\n    at C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\core\\lib\\config\\full.js:157:21\n    at Generator.next (<anonymous>)\n    at loadFullConfig (C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\core\\lib\\config\\full.js:113:5)\n    at loadFullConfig.next (<anonymous>)\n    at transform (C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\core\\lib\\transform.js:20:45)\n    at transform.next (<anonymous>)\n    at step (C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:269:25)\n    at C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:273:13\n    at async.call.result.err.err (C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:223:11)\n    at C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:189:28\n    at C:\\OSPanel\\domains\\ajax\\node_modules\\@babel\\core\\lib\\gensync-utils\\async.js:68:7\n    at C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:113:33\n    at step (C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:287:14)\n    at C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:273:13\n    at async.call.result.err.err (C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:223:11)\n    at C:\\OSPanel\\domains\\ajax\\node_modules\\gensync\\index.js:37:40");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./js/scripts.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
